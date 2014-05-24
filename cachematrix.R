6f84a1c103e66b8208589595c1687c3e3d927e2a
qzhi <kikiQZ@users.noreply.github.com>
2014-05-24 09:56:26 -0700

    Update cachematrix.R
    Add a few comments to explain my code

--- cachematrix.R
+++ cachematrix.R
@@ -3,19 +3,19 @@
 
 ## Write a short comment describing this function
 
-##Part 1
+##Part 1 makeCacheMatrix function to create an unique matrix
 ##create a new function in studio and set value of the matrix
 makeCacheMatrix <- function(x=matrix()) {
-  ##use i represent inverse
+  ##use i represent inverse; assign NULL to i
   i <- NULL
-  
+  ##create function and assign values in another environment
   set <- function(y) {
     x <<- y
     i <<- NULL
   }
   ##get the value of the matrix
   get <- function() x
-  ##set the value of the inverse
+  ##set the value of inverse
   seti <- function(inverse) i<<- inverse
   ##get the value of the inverse
   geti <- function() i
@@ -28,8 +28,9 @@ makeCacheMatrix <- function(x=matrix()) {
 
 ## Write a short comment describing this function
 
-##Part 2
+##Part 2 cacheSolve function to test and computate the inverse from cache
 cacheSolve <- function(x,...) {
+  ##get inverse value from cache if any
   i <- x$geti()
   ##check if the inverse has already been calculated
   if(!is.null(i)) {
